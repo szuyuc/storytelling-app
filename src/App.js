30a2767783578cb40b5e96b9e44548830dc6e3b4
szuyuc <szuyuc@andrew.cmu.edu>
2020-02-06 20:51:16 +0000

    deploy to heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Tweet from './Tweet';
+// one component 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div >
+      <div className="header">
+        <h1>Once upon a time..</h1>
+      </div>
+      <div className='app'>
+
+        <Tweet name='Yes' messages='starting from yes' />
+        <Tweet name='No' messages='and get a big no' />
+        <Tweet name='Ngoh' messages="wait a minute... it's actually 'Ngoh'" />
+        <Tweet name='Clarence' messages="just realize it's Clarence Ngoh" />
+        <Tweet name='Feedback' messages="What a great pun!" />
+      </div>
+
     </div>
+
   );
+
+
+
 }
 
 export default App;
